e5bec400a73b56633c145750dc3fc8b72fdae7c0
Joseph Bermudez <josephbermudez13@gmail.com>
2021-09-15 15:52:34 -0400

    Created Table.jsx
    Created class in order to create a chart with whatever dataset.

---
+++ src/common/table.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TableHeader from '../common/tableHeader';
+import TableBody from '../common/tableBody';
+
+
+const Table = ({ onSort, sortColumn, columns, data }) =>
+{
+    return (
+        <table className="table table-striped table-dark">
+            <TableHeader
+                onSort={ onSort }
+                sortColumn={ sortColumn }
+                columns={ columns }
+            />
+            <TableBody
+                data={ data }
+                columns={ columns }
+            />
+        </table>
+    );
+};
+
+export default Table;
